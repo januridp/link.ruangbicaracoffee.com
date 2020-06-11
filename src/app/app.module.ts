a5a2c3e3e289760eafa93f3cb72cfccdfc68027c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-11 21:39:21 +0000

    Deploy januridp/link.ruangbicaracoffee.com to github.com/januridp/link.ruangbicaracoffee.com.git:gh-pages

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    FontAwesomeModule
+  ],
+  providers: [],
+  bootstrap: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
+  ]
+})
+export class AppModule { }
